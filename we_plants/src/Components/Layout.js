77925095c8aefcdc71e6bdcf8781f8386c70058e
Niv Swisa <nivswisa9@gmail.com>
2021-01-28 16:25:38 +0200

    Layout done

--- we_plants/src/Components/Layout.js
+++ we_plants/src/Components/Layout.js
@@ -1,1 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import backgroundImage from '../Images/BackGroundImage.png';
+
+const styles = {
+    backGroundImage : {
+        position: "relative",
+        width : '100%',
+        height : '100%',
+    }
+}
+
+const Layout = () => {
+
+    
+    return (
+        <div style={styles.backGroundImage}>
+            <img src={backgroundImage} alt="backgroundImage" />
+        </div>
+    )
+}
+export default Layout;
